292c02560ea5f53aa19524065365236427c78acb
jayeshbabuav <jayeshbabuav@gmail.com>
2020-08-06 20:54:17 +0530

    Complete project

--- app/src/androidTest/java/in/coding/experiononlinetest/MainActivityUITest.kt
+++ app/src/androidTest/java/in/coding/experiononlinetest/MainActivityUITest.kt
@@ -13,7 +13,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4ClassRunner::class)
-class MainActivityUnitTest {
+class MainActivityUITest {
 
     @get: Rule
     val activityRule = ActivityScenarioRule(MainActivity::class.java)
